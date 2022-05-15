9a27ad5e23081e87ea77e41303720d741d522676
awp-finanznachrichten <sw@awp.ch>
2022-05-15 13:36:27 +0200

    commit from Rstudio

--- organspende_korrelation.R
+++ organspende_korrelation.R
@@ -1,6 +1,7 @@
 library(readxl)
-organspender_korrelation <- read_excel("Data/organe_korrelation.xlsx")
+organspender_korrelation <- na.omit(read_excel("Data/organe_korrelation.xlsx"))
+
+
 
 (cor(organspender_korrelation$Organspender,organspender_korrelation$Transplantation))^2
-
 
